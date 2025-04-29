4d3656103643e4afa825b90c44f9e5921a7cdbb9
Mrigaank-9 <130607343+Mrigaank-9@users.noreply.github.com>
2025-04-29 16:52:55 +0530

    Added code for miniProject

---
+++ MiniProject/src/test/java/com/leo/chatSpec/ChatSpecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.chatSpec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatSpecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
